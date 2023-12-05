d7988e5591a71f465d68efcfe042f4dfd6308f30
Raphael Charles <106124732+R-Charles@users.noreply.github.com>
2023-12-05 08:44:04 -0800

    added algo

---
+++ Algorithms/algos28print255.js
@@ -0,0 +1,13 @@
+// Print all integers from 1 to 255
+
+/*  Step1 create an empty array 
+    Step2 generate values from 0 to 255 
+    Step3 add each value to our empty array 
+*/
+
+let arr = [];
+for (val = 0; val <= 255; val++)
+{
+    arr.push(val)
+}
+console.log(arr)
